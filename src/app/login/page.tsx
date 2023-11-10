6b4e13d86f159c6018296a88a35835f0b4fc0784
TRoYHD <dark_angle.2010@hotmail.com>
2023-11-10 23:34:42 +0200

    adding pages and basic structure for app

---
+++ src/app/login/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const loginPage = () => {
+  return (
+    <div>login Page LA</div>
+  )
+}
+
+export default loginPage
---
+++ src/app/menu/[category]/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function categoryPage() {
+  return (
+    <div>category Page !</div>
+  )
+}
+
+export default categoryPage
